f6f5736ba17fe643541f038241389f0d7ee2f6a0
Siddharth Saha <siddharthsaha@Webonises-Macbook-Pro.local>
2017-01-13 13:15:19 +0530

    titanic project completed

--- udemy-data-science-course/9-data-projects/1-titanic-project.py
+++ udemy-data-science-course/9-data-projects/1-titanic-project.py
@@ -96,3 +96,124 @@ df['Age'].hist(bins = 70)
 plt.show()
 
 print 'Mean of Age: {}'.format(df['Age'].mean())
+
+# FacetGrids to show KDE plot for Ages for different Sexes
+fig = sns.FacetGrid(df, hue = 'Sex', aspect = 4)
+fig.map(sns.kdeplot, 'Age', shade = True)
+oldest = df['Age'].max()
+fig.set(xlim = (0, oldest))
+fig.add_legend()
+plt.show()
+
+# FacetGrid to show KDE plot for Person for different Sexes but showing histograms
+fig = sns.FacetGrid(df, hue = 'Person', aspect = 4)
+fig.map(sns.kdeplot, 'Age', shade = True)
+fig.set(xlim = (0, oldest))
+fig.add_legend()
+plt.show()
+
+# Age range by Pclass
+fig = sns.FacetGrid(df, hue = 'Pclass', aspect = 4)
+fig.map(sns.kdeplot, 'Age', shade = True)
+fig.set(xlim = (0, oldest))
+fig.add_legend()
+plt.show()
+
+## Question - What deck where the passengers were on and how it relates to class?
+
+deck = df['Cabin'].dropna()
+print deck.head()
+
+def grab_deck_letter(deck):
+    if deck is not np.NaN:
+	return '{}'.format(deck[0])
+    return np.NaN
+
+deck = deck.apply(grab_deck_letter)
+cabin_df = DataFrame(deck)
+cabin_df = cabin_df[cabin_df['Cabin'] != 'T']
+
+sns.factorplot(x = 'Cabin', data = cabin_df, palette = 'summer', kind = 'count')
+plt.show()
+
+# How does age vary for different decks
+
+df['Deck'] = df['Cabin'].apply(grab_deck_letter)
+df = df[df['Deck'] != 'T']
+print df.head()
+fig = sns.FacetGrid(df, hue = 'Deck', aspect = 4)
+fig.map(sns.kdeplot, 'Age', shade = True)
+fig.set(xlim = (0, oldest))
+fig.add_legend()
+plt.show()
+
+sns.factorplot(x = 'Embarked', hue = 'Pclass', data = df, kind = 'count',
+	      order = ['C', 'Q', 'S'])
+plt.show()
+
+# Who was alone and who were with family?
+def was_with_family(family_members):
+    siblings, parents_children = family_members
+    siblings = int(siblings)
+    parents_children = int(parents_children)
+
+    if siblings > 0 or parents_children > 0:
+	return 'With Family'
+    return 'Without Family'
+
+df['Family'] = df[['SibSp', 'Parch']].apply(was_with_family, axis = 1)
+print df.head()
+sns.factorplot(x = 'Family', data = df, kind = 'count')
+plt.show()
+
+# Which class has more families
+sns.factorplot(x = 'Family', hue = 'Pclass', data = df, kind = 'count')
+plt.show()
+
+# What factors help someone survive the sinking of Titanic?
+df['Survivor'] = df['Survived'].map({0 : 'No', 1 : 'Yes'})
+print df.head()
+
+sns.factorplot(x = 'Survivor', data = df, kind = 'count')
+plt.show()
+
+# Was class the factor for survival?
+sns.factorplot(x = 'Pclass', y = 'Survived', data = df)
+plt.show()
+
+sns.factorplot(x = 'Pclass', y = 'Survived', hue = 'Person', data = df)
+plt.show()
+
+# whether age was a factor
+sns.lmplot(x = 'Age', y = 'Survived', data = df)
+plt.show()
+
+# effect of class and age
+sns.lmplot(x = 'Age', y = 'Survived', hue = 'Pclass', data = df,
+	  palette = 'winter')
+plt.show()
+
+# Bin Ages
+generations = [10, 20, 40, 60, 80]
+sns.lmplot(x = 'Age', y = 'Survived', hue = 'Pclass', data = df,
+	  palette = 'winter', x_bins = generations)
+plt.show()
+
+# How gender affects
+sns.lmplot(x = 'Age', y = 'Survived', hue = 'Sex', data = df,
+	  palette = 'winter', x_bins = generations)
+plt.show()
+
+# Did the deck have an effect on survival? And how does being a male affect this?
+sns.factorplot(x = 'Deck', y = 'Survived', hue = 'Sex', data = df,
+	      palette = 'winter', kind = 'bar')
+plt.show()
+
+# Did having a family member increase the survival?
+sns.factorplot(x = 'Family', y = 'Survived', data = df, palette = 'winter',
+	      kind = 'bar')
+plt.show()
+
+sns.factorplot(x = 'Family', y = 'Survived', hue = 'Sex', data = df, palette = 'winter',
+	      kind = 'bar')
+plt.show()
